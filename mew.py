80555e7c272f30eaed8dfa35800737febb7a785e
Lencerf <changyuan.lv@gmail.com>
2018-03-04 15:00:29 -0500

    almost rewrite it

--- mew.py
+++ mew.py
@@ -2,105 +2,141 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import pickle
+import yaml
 from numpy.random import permutation
+from general_codes.utils import sayFinished
 
-def testm2w(word_dict):
-    full_score = {w:len(word_dict[w]["meanings"]) for w in word_dict}
+def red_text(text):
+    return f'\033[31m {text}\033[00m'
+def green_text(text):
+    return f'\033[32m {text}\033[00m'
+def bold_text(text):
+    return f"\033[1m{text}\033[0m"
+
+def save_exit(database, word_to_learn):
+    print('exiting...')
+    for word_at_list, items in word_to_learn.items():
+        word, list_name = word_at_list.split("@", 1)
+        database[list_name][word] = items
+    with open(sys.argv[1], 'w', encoding='utf-8') as f:
+        yaml.dump(database, f, allow_unicode=True)
+    exit(0)
+
+def review(database, word_to_learn):
+    word_to_learn_array = [(word_at_list, items) 
+        for word_at_list, items in word_to_learn.items()]
+    word_to_learn_array.sort(key=lambda w:(w[1]["f"], w[1]['h']), reverse=True)
+    for word_at_list, items in word_to_learn_array:
+        word = word_at_list.split("@", 1)[0]
+        print(f'{word}, frequency: {items["f"]}, h:{items["h"]}')
+        for m in items["m"]:
+            print("  ", m, end="")
+        sayFinished(word)
+        if input('  ') == '':
+            continue
+        else:
+            break
+
+def dictation(database, word_to_learn):
+    test_word(database, word_to_learn, say_meaning=True)
+
+def test_word(database, word_to_learn, say_meaning=False):
+    full_score = {w:0 for w in word_to_learn}
     study_list_freq = {}  # frequency: wrodlist
-    for (w, v) in word_dict.items():
-        for m in v["meanings"]:
-            if v["frequency"] in study_list_freq:
-                study_list_freq[v["frequency"]].append(f"{m}@{w}")
-            else:
-                study_list_freq[v["frequency"]] = [f"{m}@{w}"]
+    for w, v in word_to_learn.items():
+        for m in v["m"]:
+            if v['f'] not in study_list_freq:
+                study_list_freq[v['f']] = {}
+            if v['h'] not in study_list_freq[v["f"]]:
+                study_list_freq[v["f"]][v['h']] = []
+            study_list_freq[v["f"]][v['h']].append(f"{m}@{w}")
     word_studied = set()
     for freq in sorted(study_list_freq.keys(), reverse=True):
-        study_list = study_list_freq[freq]
-        print(f'start frequency {freq}')
-        breaked = False
-        for index in permutation(len(study_list)):
-            meaning, word = study_list[index].split("@", maxsplit=2)
-            word_input = input(f"{meaning}: ")
-            if word == word_input:
-                print("Correct!")
-                full_score[word] -= 1
-            elif word_input == "STOP":
-                breaked = True
+        print(bold_text(f'start frequency {freq}'))
+        broken = False
+        for historical_h in sorted(study_list_freq[freq].keys(), reverse=True):    
+            study_list = study_list_freq[freq][historical_h]
+            for index in permutation(len(study_list)):
+                meaning, word, at_list = study_list[index].split("@", maxsplit=2)
+                if say_meaning:
+                    sayFinished(meaning)
+                else:
+                    print(f"{meaning}: ", end='')
+                word_input = input()
+                if word == word_input:
+                    print(green_text("Correct!"), f"h={historical_h}")
+                elif word_input == "STOP":
+                    broken = True
+                    current_word_at_list = f"{word}@{at_list}"
+                    if current_word_at_list in word_studied and full_score[current_word_at_list] == 0:
+                        word_studied.remove(current_word_at_list)
+                    break
+                else:
+                    full_score[f"{word}@{at_list}"] -= 1
+                    print(f"{red_text('Wrong!')} {meaning} <-> {word}")
+                word_studied.add(f"{word}@{at_list}")
+            if broken:
                 break
-            else:
-                print(f"Wrong! {meaning} <-> {word}")
-            word_studied.add(word)
-        if breaked:
+        if broken:
             break
         else:
-            print(f'end frequency {freq}')
+            print(bold_text(f'end frequency {freq}'))
     for word in word_studied:
+        #print(full_score[word])
         if full_score[word] == 0:
-            if word_dict[word]["frequency"] > 0:
-                word_dict[word]["frequency"] -= 1
+            if word_to_learn[word]['f'] > 0:
+                word_to_learn[word]['f'] -= 1
         else:
-            word_dict[word]["frequency"] += 1
+            word_to_learn[word]['f'] += 1
+            word_to_learn[word]['h'] += 1
 
-def review(word_dict):
-    for w in word_dict:
-        print(f'{w}, frequency: {word_dict[w]["frequency"]}')
-        for m in word_dict[w]["meanings"]:
-            print("  ", m, end="")
-        print()
-
-def record(word_dict):
-    while True:
-        print("new word (imput 'STOP' to stop): ")
-        text = input()
-        if text == "STOP":
-            print('stopped')
-            print(word_dict)
-            break
-        else:
-            text = text.split(' ')
-            word = text[0]
-            meanings = text[1:]
-            if word in word_dict:
-                word_dict[word]["frequency"] += 1
-                for m in meanings:
-                    word_dict[word]["meanings"].add(m)
-            else:
-                word_dict[word] = {
-                    "frequency": 1,
-                    "meanings": set(meanings)
-                }
+FUNCTION_TABLE = [
+    {
+        "name": "review",
+        "func": review
+    },{
+        "name": "exit",
+        "func": save_exit
+    },{
+        "name": "test",
+        "func": test_word
+    } 
+]
 
 def main():
     database_file_path = sys.argv[1]
-    database_file = open(database_file_path, 'rb')
-    database = pickle.load(database_file)
+    database_file = open(database_file_path, 'r')
+    database = yaml.load(database_file)
     database_file.close()
     if len(database.keys()) > 0:
-        choosed_dict = ""
-        while choosed_dict not in database:  
-            print("choose a dictionary:")
-            for key in database.keys():
-                print("  ", key)
-            choosed_dict = input()
+        print("choose a dictionary or type in a new dictionary name:")
+        for key in database.keys():
+            print("\t", key)
+        chosen_dict_name = input()
+        if chosen_dict_name not in database and chosen_dict_name != "ALL":  
+            database[chosen_dict_name] = {}
+            print(f'new dictionary {chosen_dict_name} created.')
     else:
         print("create a new dictionary:")
-        choosed_dict = input()
-        database = {choosed_dict:{}}
+        chosen_dict_name = input()
+        database = {chosen_dict_name:{}}
+        print(f'new dictionary {chosen_dict_name} created.')
+    word_to_learn = {}
+    if chosen_dict_name != 'ALL':
+        for word, items in database[chosen_dict_name].items():
+            word_to_learn[f'{word}@{chosen_dict_name}'] = items
+    else:
+        for list_name, list_content in database.items():
+            for word, items in list_content.items():
+                word_to_learn[f'{word}@{list_name}'] = items
     while True:
-        print("what do you want to do?\n\trecord\n\treview\n\ttestM2W")
-        action = input()
-        if action == "exit":
-            print('exiting...')
-            with open(database_file_path, 'wb') as f:
-                pickle.dump(database, f)
-            break
-        elif action == "record":
-            record(database[choosed_dict])
-        elif action == "testM2W":
-            testm2w(database[choosed_dict])
-        elif action == "review":
-            review(database[choosed_dict])
+        print("what do you want to do?")
+        for index, function in enumerate(FUNCTION_TABLE):
+            print(f'\t{index}:',  function['name'])
+        chosen_func_index = int(input())
+        if chosen_func_index < len(FUNCTION_TABLE):
+            FUNCTION_TABLE[chosen_func_index]['func'](database, word_to_learn)
+    
 
 if __name__ == '__main__':
     main()
