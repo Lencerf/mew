8a956b2537be634fbdd13de1dbc4c75660ecbba1
Lencerf <changyuan.lv@gmail.com>
2018-03-21 15:31:49 -0400

    generate dict from file

--- mew_gen.py
+++ mew_gen.py
@@ -1,16 +1,23 @@
 import yaml
+import sys
 
-word_text = '''ansatz 假设
-enzyme 酶'''
+word_text = ""
 
-list_name = 'list1'
+filename = sys.argv[1]
+list_name = filename.split("/").pop()
+
+with open(sys.argv[1], 'r') as f:
+    word_text = f.read()
 
 word_list = {}
 
 for line in word_text.split('\n'):
-    wm = line.split(' ')
+    wm = list(filter(lambda s:len(s)>0, line.split(' ')))
+    if len(wm) < 2:
+        continue
     word_list[wm[0]] = {
         'f':1,
-        'm':wm[1:]
+        'm':wm[1:],
+        'h':1
     }
 print(yaml.dump({list_name:word_list}, allow_unicode=True)) 
